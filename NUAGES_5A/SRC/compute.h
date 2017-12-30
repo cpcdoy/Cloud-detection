533465aa7c7758eed8f7c57dea292bb038c7862e
Axel Mendoza <axel.mendoza@epita.fr>
2017-12-30 16:29:49 +0100

    add radiometry vector array

--- NUAGES_5A/SRC/compute.h
+++ NUAGES_5A/SRC/compute.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <gtk/gtk.h>
-
+#define RAD_VECT_SIZE 5
 #define LargeurImage 360
 
 void ComputeImage(guchar *pucImaOrig,
@@ -20,3 +20,4 @@ enum neighbor_loc
 
 guchar get_pixel(guchar *img, enum neighbor_loc loc, int curr_loc);
 guchar get_pixel_radiometry(guchar *img, int curr_loc);
+void compute_radiometry(guchar (*radiometry_img)[RAD_VECT_SIZE], guchar *img, int nb_cols, int nb_lines, int rad_vect_size);
